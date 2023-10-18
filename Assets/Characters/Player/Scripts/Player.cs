74886190a7a6586c3f69833e7f24569ac43fb894
Alejandro Delati Mik <aledelati@gmail.com>
2023-10-18 10:55:21 -0300

    v0.1.8
    Implementacion de Hud para Vida del jugador y Menu Principal del Juego

--- Assets/Characters/Player/Scripts/Player.cs
+++ Assets/Characters/Player/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
@@ -15,13 +16,24 @@ public class Player : MonoBehaviour {
     private SpriteRenderer _SpriteRenderer;
     private Health _Health;
     private PlayerInput _PlayerInput;
+    private float currentHealth;
 
     [SerializeField] private PlayerProfile _PlayerProfile;  //Referencia al perfil del jugador
     [SerializeField] public PlayerProfile PlayerProfile { get => _PlayerProfile; }
 
+    //              ----|Events|----
+    [SerializeField]
+    private UnityEvent<float> OnLivesChanged;
+
     //              ----|Functions|----
+    private void Start() {
+        OnLivesChanged.Invoke(_Health.GetHP());
+        currentHealth = _Health.GetHP();
+    }
+
     private void Update() {
         PlayerSpriteUpdate();
+        PlayerHealthBarUpdate();
     }
 
     private void OnEnable() {           //Se ejecuta cuando el objeto se activa en el nivel
@@ -35,6 +47,13 @@ public class Player : MonoBehaviour {
     private void PlayerSpriteUpdate() { //Actualiza el Sprite del jugador dependiendo de la cantidad de hp restante
         _SpriteRenderer.sprite = playerSprites[(int)_Health.GetHP() - 1];
         _PlayerSprite = _SpriteRenderer.sprite;     //Mantiene actualizado cual es el sprite actual del jugador
+    }
+
+    private void PlayerHealthBarUpdate() {  //Llama al evento que actualiza el Hud de la vida del jugador al detectar cambios en el valor de HP
+        if(currentHealth != _Health.GetHP()) {
+            currentHealth = _Health.GetHP();
+            OnLivesChanged?.Invoke(_Health.GetHP());
+        }
     }
 
 }
---
+++ Assets/Scripts/Hud Scripts/HUD Controller.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour {
+
+    [SerializeField] GameObject healthIcon;
+    [SerializeField] GameObject playerHealthContainter;
+
+    public void UpdateHPHud(float HP) {
+        Debug.Log("Hud HP Update");
+        if (HealthContainerQuantity() == 0) {
+            LoadHealthContainer(HP);
+            return;
+        }
+
+        if(HealthContainerQuantity() > HP) {
+            RemoveHealthIcon();
+        } else {
+            AddHealthIcon();
+        }
+
+    }
+
+    private int HealthContainerQuantity() {
+        return playerHealthContainter.transform.childCount;
+    }
+
+
+
+    private void LoadHealthContainer(float Quantity) {
+        for(int i = 0; i < Quantity; i++) {
+            AddHealthIcon();
+        }
+    }
+
+    private void AddHealthIcon() {
+        Instantiate(healthIcon, playerHealthContainter.transform);
+    }
+
+    private void RemoveHealthIcon() {
+        Transform container = playerHealthContainter.transform;
+        GameObject.Destroy(container.GetChild(container.childCount - 1).gameObject);
+    }
+
+}
